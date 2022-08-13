4a809bbd0867187e6c9c5aebefe7f83773483693
Bishnu <bishnuthapako@gmail.com>
2022-08-13 07:51:04 -0400

    completed database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,18 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/users" do
+    user = User.all
+    user.to_json(include: :messages)
+    # { message: "Good luck with your project!" }.to_json
+  end
+
+  get "/users" do
+    users = []
+    5.times do 
+      users << User.all.sample
+    end
+    users.to_json(include: :messages)
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,22 @@
+require 'faker'
+
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+10.times do 
+    user = User.create(
+        username: Faker::Name.first_name,
+    )
+    
+    rand(1..3).times do
+        Message.create(
+            title: Faker::Book.title,
+            comment: Faker::Lorem.sentence,
+            user_id: user.id
+        )
+
+    end
+ 
+end
 
 puts "✅ Done seeding!"
