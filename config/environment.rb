c320f774b07a1ad3a88de01c8daea8573270fda7
Bishnu <bishnuthapako@gmail.com>
2022-08-23 16:52:37 -0400

    heroku deployed

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,11 @@
 # This is an _environment variable_ that is used by some of the Rake tasks to determine
 # if our application is running locally in development, in a test environment, or in production
+# postgres branch push upstream
 ENV['RACK_ENV'] ||= "development"
 
 # Require in Gems
 require 'bundler/setup'
+# require 'pg'
 Bundler.require(:default, ENV['RACK_ENV'])
 
 # Require in all files in 'app' directory
