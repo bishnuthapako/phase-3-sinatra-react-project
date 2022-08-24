a7d613d5c038b69fd4979d540d4619ac00067bf3
Bishnu <bishnuthapako@gmail.com>
2022-08-24 04:27:29 -0400

    update application_controller file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,11 @@ class ApplicationController < Sinatra::Base
     # { message: "Good luck with your project!" }.to_json
   end
 
+  get "/message" do
+    messages = Message.all
+    messages.to_json
+  end
+
   get "/users" do
     users = []
     5.times do 
@@ -15,5 +20,49 @@ class ApplicationController < Sinatra::Base
     end
     users.to_json(include: :messages)
   end
+
+  get "/users/:id" do
+    users = User.find(params[:id])
+    users.to_json
+  end
+
+  patch "/message/:id" do
+    message = Message.find(params[:id])
+    message.update(
+      title: params[:title],
+      comment: params[:comment]
+    )
+    message.to_json
+  end
+
+  delete "/users/:id" do
+    users = User.find(params[:id])
+    users.destroy
+    users.to_json
+  end
+  
+  post "/users" do
+      user = User.create(
+      username = params[:username]
+    )
+  end
+
+  post "/message" do
+    user = User.find_or_create_by(username: params[:username])
+    message = Message.create(
+    user_id: user.id,
+    title: params[:title],
+    comment: params[:comment],
+    )
+    message.to_json
+  end
+
+  delete "/message/:id" do
+      message = Message.find(params[:id])
+      message.destroy
+      message.to_json
+   end
+
+
 
 end
